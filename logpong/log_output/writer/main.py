e62c558a711bee9731820e28ac2be05e2dfc3f4b
Niklas Linnanen <nlinnanen@gmail.com>
2025-12-25 23:41:17 +0200

    Overwrite instead of appending the log

--- logpong/log_output/writer/main.py
+++ logpong/log_output/writer/main.py
@@ -8,6 +8,6 @@ import os
 str = uuid.uuid4() 
 while True:
     out_path = os.path.expanduser(os.path.expandvars(os.environ.get("OUTPUT_PATH", "output.txt")))
-    with open(out_path, "a", encoding="utf-8") as f:
+    with open(out_path, "w", encoding="utf-8") as f:
       f.write(f"{dt.now()} {str}\n")
     time.sleep(5)
